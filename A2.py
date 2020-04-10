ac9bb25ad4dd3c2b006d57c355a20f738415ee6b
wclemons <wclemons@csu.fullerton.edu>
2020-04-10 16:57:06 -0700

    squeaky clean

--- A2.py
+++ A2.py
@@ -3,8 +3,6 @@ import string
 
 from Crypto.Cipher import DES
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from Crypto.Util.Padding import unpad
 from Crypto import Random
 
 global x, y
@@ -24,13 +22,8 @@ class CipherInterface:
         else:
             print("Invalid Key")
 
-    def ctol(self, string):
-        string.encode('ascii')
-        print(string)
-        return string
-
-    def ltoc(self, data):
-        pass
+    def pad(self, message, block_size):
+        return message + b"\0" * (block_size - len(message) % block_size)
 
 class AES_cipher(CipherInterface):
     def __init__(self):
@@ -42,7 +35,7 @@ class AES_cipher(CipherInterface):
         print("block size: ", block_size)
         # encrypt and add padding if neccessary
         if len(buffer) % 16 != 0:
-            ciphertext = y.encrypt(pad(buffer, block_size))
+            ciphertext = y.encrypt(self.pad(buffer, block_size))
         else:
             ciphertext = y.encrypt(buffer)
         print("ciphertext: ", ciphertext)
@@ -51,11 +44,14 @@ class AES_cipher(CipherInterface):
     def decrypt(self, buffer, block_size):
         print("buffer: ", buffer)
         print("block size: ", block_size)
-        # decrypt and add padding if neccessary
-        if len(buffer) % 16 != 0:
-            plaintext = unpad(y.decrypt(buffer), block_size)
-        else:
-            plaintext = y.decrypt(buffer)
+        
+        # decrypt 
+        plaintext = y.decrypt(buffer)
+
+        # remove padded bytes from plaintext
+        if '\0' in plaintext.decode('ASCII'):
+            plaintext = plaintext.rstrip(b'\0')
+
         print("plaintext: ", plaintext)
         return plaintext
 
@@ -64,67 +60,35 @@ class DES_cipher(CipherInterface):
     def __init__(self):
         super(DES_cipher, self).__init__()
 
-    # def encrypt(self, lists, key):
     def encrypt(self, buffer, block_size):
         print("buffer: ", buffer)
         print("buffer size: ", len(buffer))
         print("block size: ", block_size)
+
         # encrypt and add padding if neccessary
-        # while len(buffer) % 8 != 0:
-            # buffer += b'0'
-        # ciphertext = x.encrypt(buffer)
-        
         if len(buffer) % 8 != 0:
-            ciphertext = x.encrypt(pad(buffer, block_size))
+            ciphertext = x.encrypt(self.pad(buffer, block_size))
         else:
             ciphertext = x.encrypt(buffer)
 
         print("ciphertext: ", ciphertext)
         return ciphertext
 
-    # def decrypt(self, output, matrix):
     def decrypt(self, buffer, block_size):
         print("buffer: ", buffer)
         print("buffer size: ", len(buffer))
         print("block size: ", block_size)
-        # decrypt and add padding if neccessary
-        # while len(buffer) % 8 != 0:
-        #     buffer += b'0'
-        # plaintext = x.decrypt(buffer)
 
-        if len(buffer) % 8 != 0:
-            plaintext = unpad(x.decrypt(buffer), block_size)
-        else:
-            plaintext = x.decrypt(buffer)        
+        # decrypt 
+        plaintext = x.decrypt(buffer)        
 
         print("plaintext before replacing padding: ", plaintext)
-        if ('\x01' in plaintext.decode('ASCII')):
-            print("padding found!")
-            plaintext = plaintext.rstrip(b'\x01')
-        elif ('\x07' in plaintext.decode('ASCII')):
-            print("space padding found!")
-            plaintext = plaintext.rstrip(b'\x07')
+        # remove padding where it's found...
+        if '\0' in plaintext.decode('ASCII'):
+            plaintext = plaintext.rstrip(b'\0')
         
         print("plaintext after replacing padding: ", plaintext)
         return plaintext
-
-# note: buffer size = 8 was in 2nd param before
-# def chunks(filename, buffer_size):
-#     print("in chunks: buffer size: ", buffer_size)
-#     with open(filename, "rb") as fp:
-#         chunk = fp.read(buffer_size)
-#         while chunk:
-#             yield chunk
-#             chunk = fp.read(buffer_size)
-
-# # note: buffer size = 8 was in 2nd param before
-# def blocks(filename, buffersize):
-#     print("in chunks: buffer size: ", buffersize)
-#     blocksize = buffersize / 2
-#     for chunk in chunks(filename, buffersize):
-#         type(chunk[:blocksize])
-#         block = [chunk[:blocksize],chunk[blocksize:]]
-#         yield block
 
 def main():
     cipher = None
@@ -172,16 +136,6 @@ def main():
     # close files
     input_file.close()
     output_file.close()
-
-    # with open(outputFile, "wb") as fp:
-    #         for block in blocks(inputFile, buffersize):
-    #             if enc == "enc":
-    #                 fp.write(cipher.encrypt(block, len(block)))
-    #             elif enc == "dec":
-    #                  fp.write(cipher.decrypt(block, len(block)))
-    #             else:
-    #                 print("Choose enc/dec")
-    #                 exit
 
 if __name__ == "__main__":
     if len(sys.argv) == 6:
